d77a2fc4611c3160b8816055552604465b240272
raoulsullivan <stephen@beyondspreadsheets.co.uk>
2014-12-29 16:31:11 +0000

    Got tastypie up and responding

--- correlator/urls.py
+++ correlator/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from . import views
+from api import EntryResource
+
+entry_resource = EntryResource()
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'correlator.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
+    (r'^api/', include(entry_resource.urls)),
 )
